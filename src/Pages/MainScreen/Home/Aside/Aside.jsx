4faba7ef243b3c958339fb4d0538a54c73f0aa8f
Sebastian Mesa <sandlerzop@gmail.com>
2022-05-16 12:33:11 -0500

    See more btn communities on the asidebar added

--- src/Pages/MainScreen/Home/Aside/Aside.jsx
+++ src/Pages/MainScreen/Home/Aside/Aside.jsx
@@ -5,17 +5,21 @@ import { isLoadingCommunities, selectCommunities } from '../Aside/AsideSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import NavCommunities from '../../../../Components/NavLinkCommunities'
 import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getCommunities } from './AsideSlice'
 import { getSearch } from '../HomeSlices'
 
 export default function Aside() {
+  const [moreCommunities, setMoreCommunities] = useState(false)
   const { term, community } = useParams()
   const loadingCommunities = useSelector(isLoadingCommunities)
   const communities = useSelector(selectCommunities)
   const dispatch = useDispatch()
 
-  const dataCommunities = communities[community || term || 'home']?.slice(0, 10)
+  const dataCommunities = communities[community || term || 'home']?.slice(
+    0,
+    moreCommunities ? 25 : 10
+  )
   const mapCommunities = dataCommunities?.map((community, index) => (
     <NavCommunities data={community} key={community.id} index={index + 1} />
   ))
@@ -35,6 +39,12 @@ export default function Aside() {
         <h2 className="aside__communities__title">Communities</h2>
         <div className="aside__communities__container">
           {loadingCommunities ? <SkeletonCommunities /> : mapCommunities}
+        </div>
+        <div
+          className="aside__communities__more-communities"
+          onClick={() => setMoreCommunities(prev => !prev)}
+        >
+          {moreCommunities ? 'Less' : 'More'} Communities
         </div>
       </div>
       <div className="aside__about">
